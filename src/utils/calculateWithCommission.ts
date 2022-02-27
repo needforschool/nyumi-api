3073e4565ad5503002abd820f733328f1381e03d
Antoine Kingue <etorefr@gmail.com>
2022-02-27 19:11:05 +0100

    🔥 remove unused utils

--- src/utils/calculateWithCommission.ts
+++
@@ -1,8 +0,0 @@
-export const calculateWithCommission = (
-  price: number,
-  commission: number
-): number => {
-  if (price === 0) return price;
-
-  return (price * commission) / 100 + price;
-};
